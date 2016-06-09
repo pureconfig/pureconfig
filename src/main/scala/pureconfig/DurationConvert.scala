9ec74438a901035ef96488e4d3a65d92ed831aac
Leif Wickland <leifwickland@gmail.com>
2016-06-09 16:39:16 -0600

    Add ConfigConvert[Duration].

---
+++ src/main/scala/pureconfig/DurationConvert.scala
@@ -0,0 +1,53 @@
+package pureconfig
+
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.duration.Duration
+import scala.util.Try
+
+/**
+ * Utility functions for converting a Duration to a String and vice versa.
+ */
+private[pureconfig] object DurationConvert {
+  /**
+   * Convert a string to a Duration using Typesafe Config's algorithm.
+   * Typesafe Config has a really nice duration parsing algorithm; unfortunately it's private.
+   * We jump through hoops to reuse that for compatibility.
+   */
+  def from(stringyDuration: String): Try[Duration] = Try {
+    val phonyKey = "k"
+    val phonyConfigString = s"$phonyKey: $stringyDuration"
+    val javaDuration = ConfigFactory.parseString(phonyConfigString).getDuration(phonyKey)
+    Duration.fromNanos(javaDuration.toNanos)
+  }
+
+  /**
+   * Format a duration as a string with a suitable time unit using units TypesafeConfig understands.
+   */
+  def from(d: Duration): String = {
+    d.toNanos match {
+      case 0L => "0"
+      case n =>
+        timeUnitsToLabels.collectFirst {
+          case (unitInNanos, unitLabel) if n >= unitInNanos && n % unitInNanos == 0 =>
+            s"${n / unitInNanos}$unitLabel"
+        }.getOrElse(s"${n}ns")
+    }
+  }
+  private final val microsecondInNanos = 1000L
+  private final val millisecondInNanos = 1000L * microsecondInNanos
+  private final val secondInNanos = 1000L * millisecondInNanos
+  private final val minuteInNanos = 60L * secondInNanos
+  private final val hourInNanos = 60L * minuteInNanos
+  private final val dayInNanos = 24L * hourInNanos
+
+  // Must be sorted from largest unit to smallest.
+  private final val timeUnitsToLabels = Vector(
+    dayInNanos -> "d",
+    hourInNanos -> "h",
+    minuteInNanos -> "m",
+    secondInNanos -> "s",
+    millisecondInNanos -> "ms",
+    microsecondInNanos -> "us"
+  ).sortBy(_._1)(implicitly[Ordering[Long]].reverse)
+}
---
+++ src/test/scala/pureconfig/DurationConvertTest.scala
@@ -0,0 +1,59 @@
+package pureconfig
+
+import java.util.concurrent.TimeUnit
+
+import org.scalatest.{ FlatSpec, Matchers, TryValues }
+
+import scala.concurrent.duration.Duration
+import DurationConvert.from
+
+import scala.util.{ Failure, Success }
+
+class DurationConvertTest extends FlatSpec with Matchers {
+  "Converting a Duration to a String" should "pick an appropriate unit when dealing with whole units less than the next step up" in {
+    from(Duration(14, TimeUnit.DAYS)) shouldBe "14d"
+    from(Duration(16, TimeUnit.HOURS)) shouldBe "16h"
+    from(Duration(1, TimeUnit.MINUTES)) shouldBe "1m"
+    from(Duration(33, TimeUnit.SECONDS)) shouldBe "33s"
+    from(Duration(555, TimeUnit.MILLISECONDS)) shouldBe "555ms"
+    from(Duration(999, TimeUnit.MICROSECONDS)) shouldBe "999us"
+    from(Duration(222, TimeUnit.NANOSECONDS)) shouldBe "222ns"
+  }
+  it should "pick an appropriate unit when dealing with whole units more than, but fractional in, the next step up" in {
+    from(Duration(1, TimeUnit.DAYS)) shouldBe "1d"
+    from(Duration(47, TimeUnit.HOURS)) shouldBe "47h"
+    from(Duration(123, TimeUnit.MINUTES)) shouldBe "123m"
+    from(Duration(489, TimeUnit.SECONDS)) shouldBe "489s"
+    from(Duration(11555, TimeUnit.MILLISECONDS)) shouldBe "11555ms"
+    from(Duration(44999, TimeUnit.MICROSECONDS)) shouldBe "44999us"
+    from(Duration(88222, TimeUnit.NANOSECONDS)) shouldBe "88222ns"
+  }
+  it should "write 0 without units" in {
+    from(Duration(0, TimeUnit.DAYS)) shouldBe "0"
+  }
+
+  "Converting a String to a Duration" should "succeed for known units" in {
+    from("1d") shouldBe Success(Duration(1, TimeUnit.DAYS))
+    from("47h") shouldBe Success(Duration(47, TimeUnit.HOURS))
+    from("123m") shouldBe Success(Duration(123, TimeUnit.MINUTES))
+    from("489s") shouldBe Success(Duration(489, TimeUnit.SECONDS))
+    from("11555ms") shouldBe Success(Duration(11555, TimeUnit.MILLISECONDS))
+    from("44999us") shouldBe Success(Duration(44999, TimeUnit.MICROSECONDS))
+    from("88222ns") shouldBe Success(Duration(88222, TimeUnit.NANOSECONDS))
+  }
+  it should "report a helpful error message when failing to convert a bad duration" in {
+    val key = "a.long.config.path"
+    val badDuration = "10 lordsALeaping"
+    val rawConfig = Map(key -> badDuration)
+    val result = LocalDurationConvert.durationConfigConvert.from(rawConfig, key)
+    result match {
+      case Success(_) => fail("Should be failure")
+      case Failure(ex) =>
+        val message = ex.getMessage
+        println(message)
+        message should include(key)
+        message should include(badDuration)
+    }
+  }
+  private object LocalDurationConvert extends LowPriorityConfigConvertImplicits
+}
