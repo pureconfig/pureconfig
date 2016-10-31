9a4ab695f2ae3012e0352f75f449b13cc32c87bd
Joao Azevedo <joao.c.azevedo@gmail.com>
2016-10-31 02:14:01 +0000

    Document the apply methods of ConfigFieldMapping

--- src/main/scala/pureconfig/ConfigFieldMapping.scala
+++ src/main/scala/pureconfig/ConfigFieldMapping.scala
@@ -6,9 +6,32 @@ trait ConfigFieldMapping[T] extends (String => String) {
 
 object ConfigFieldMapping extends LowPriorityConfigFieldMappingImplicits {
   def apply[T](implicit mapping: ConfigFieldMapping[T]): ConfigFieldMapping[T] = mapping
+
+  /**
+   * Creates a ConfigFieldMapping from the provided function, mapping names in
+   * the object that will receive config values to names in the source
+   * configuration.
+   *
+   * @param f a function that maps names in the object that will receive config
+   *        values to names in the source configuration
+   * @return a ConfigFieldMapping created from the provided function.
+   */
   def apply[T](f: String => String): ConfigFieldMapping[T] = new ConfigFieldMapping[T] {
     def apply(fieldName: String): String = f(fieldName)
   }
+
+  /**
+   * Creates a ConfigFieldMapping according to the naming conventions specified
+   * both for the object that will receive config values and for the source
+   * configuration.
+   *
+   * @param typeFieldConvention naming convention used by the fields of the
+   *        object which will receive config values
+   * @param configFieldConvention naming convention used in the source
+   *        configuration (e.g. the config file)
+   * @return a ConfigFieldMapping created according to the provided naming
+   *         conventions.
+   */
   def apply[T](typeFieldConvention: NamingConvention, configFieldConvention: NamingConvention): ConfigFieldMapping[T] = {
     if (typeFieldConvention == configFieldConvention) {
       apply(identity(_))
