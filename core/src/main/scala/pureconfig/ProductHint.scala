4b66c1e30ded04521d471dd3af9bd068d867d083
Rui Gonçalves <ruippeixotog@gmail.com>
2017-01-21 00:32:34 +0000

    Add Scaladoc to ProductHint

--- core/src/main/scala/pureconfig/ProductHint.scala
+++ core/src/main/scala/pureconfig/ProductHint.scala
@@ -1,8 +1,25 @@
 package pureconfig
 
+/**
+ * A trait that can be implemented to customize how case classes are read from and written to a config.
+ *
+ * @tparam T the type of case class for which this hint applies
+ */
 trait ProductHint[T] {
+
+  /**
+   * The mapping used to map field names to config keys
+   */
   def fieldMapping: ConfigFieldMapping
+
+  /**
+   * A boolean indicating if the default arguments of the case class should be used when fields are missing
+   */
   def useDefaultArgs: Boolean
+
+  /**
+   * A boolean indicating if config keys that do not map to a case class field are allowed in config objects
+   */
   def allowUnknownKeys: Boolean
 }
 
