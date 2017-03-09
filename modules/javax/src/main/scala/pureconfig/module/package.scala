3bb3019d10e8892f672b95adcc84280e97e5c044
Derek Morr <derek.morr.test@gmail.com>
2017-03-08 22:41:34 -0500

    add X500Principal

--- modules/javax/src/main/scala/pureconfig/module/package.scala
+++ modules/javax/src/main/scala/pureconfig/module/package.scala
@@ -1,6 +1,7 @@
 package pureconfig.module
 
 import _root_.javax.security.auth.kerberos.KerberosPrincipal
+import _root_.javax.security.auth.x500.X500Principal
 
 import scala.util.Try
 
@@ -13,4 +14,7 @@ import pureconfig.ConfigConvert.fromStringConvertTry
 package object javax {
   implicit val readKerberosPrincipal: ConfigConvert[KerberosPrincipal] =
     fromStringConvertTry[KerberosPrincipal](s => Try(new KerberosPrincipal(s)), _.toString)
+
+  implicit val readX500Principal: ConfigConvert[X500Principal] =
+    fromStringConvertTry[X500Principal](s => Try(new X500Principal(s)), _.toString)
 }
