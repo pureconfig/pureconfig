aaf431c36d2f734e2df3f088816358ffd6f5c3a4
Derek Morr <derek.morr.test@gmail.com>
2017-03-10 20:42:16 -0500

    add javax module to README; switch javax to catchReadError

--- modules/javax/src/main/scala/pureconfig/module/package.scala
+++ modules/javax/src/main/scala/pureconfig/module/package.scala
@@ -6,15 +6,15 @@ import _root_.javax.security.auth.x500.X500Principal
 import scala.util.Try
 
 import pureconfig.ConfigConvert
-import pureconfig.ConfigConvert.fromStringConvertTry
+import pureconfig.ConfigConvert.{ catchReadError, fromStringConvert }
 
 /**
  * ConfigConvert instances for javax value classes.
  */
 package object javax {
   implicit val readKerberosPrincipal: ConfigConvert[KerberosPrincipal] =
-    fromStringConvertTry[KerberosPrincipal](s => Try(new KerberosPrincipal(s)), _.toString)
+    fromStringConvert[KerberosPrincipal](catchReadError(s => new KerberosPrincipal(s)), _.toString)
 
   implicit val readX500Principal: ConfigConvert[X500Principal] =
-    fromStringConvertTry[X500Principal](s => Try(new X500Principal(s)), _.toString)
+    fromStringConvert[X500Principal](catchReadError(s => new X500Principal(s)), _.toString)
 }
