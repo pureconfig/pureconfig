16177dd6773e59860254621bb879ebe1041cdca7
melrief <mario.pastorelli@teralytics.ch>
2017-03-20 17:37:27 +0100

    ProductConvertersSuite refactored

--- core/src/test/scala/pureconfig/ApiSuite.scala
+++ core/src/test/scala/pureconfig/ApiSuite.scala
@@ -97,4 +97,63 @@ class ApiSuite extends FlatSpec with Matchers with OptionValues with EitherValue
   //    loadConfigFromFiles[FlatConfig](files) shouldBe a[Left[_, _]]
   //  }
 
+  //  it should s"be able to save ${classOf[Config2]} and load ${classOf[Config]} when namespace is set to config" in {
+  //    withTempFile { configFile =>
+  //      val expectedConfig = Config(new DateTime(1), List(1, 2, 3), Set(4, 5, 6), FlatConfig(false, 1d, 2f, 3, 4l, "5", Option("6")))
+  //      val configToSave = Config2(expectedConfig)
+  //      saveConfigAsPropertyFile(configToSave, configFile, overrideOutputPath = true)
+  //      val config = loadConfig[Config](configFile, "config")
+  //
+  //      config should be(Right(expectedConfig))
+  //    }
+  //  }
+  //
+  // a realistic example of configuration: common available Spark properties
+  //  case class DriverConf(cores: Int, maxResultSize: String, memory: String)
+  //  case class ExecutorConf(memory: String, extraJavaOptions: String)
+  //  case class SparkAppConf(name: String)
+  //  case class SparkLocalConf(dir: String)
+  //  case class SparkNetwork(timeout: FiniteDuration)
+  //  case class SparkConf(master: String, app: SparkAppConf, local: SparkLocalConf, driver: DriverConf, executor: ExecutorConf, extraListeners: Seq[String], network: SparkNetwork)
+  //  case class SparkRootConf(spark: SparkConf)
+
+  //  // ** save and load, product
+  //  it should s"be able to save and load ${classOf[SparkRootConf]}" in {
+  //    withTempFile { configFile =>
+  //
+  //      val writer = new PrintWriter(Files.newOutputStream(configFile))
+  //      writer.println("""spark.executor.extraJavaOptions=""""")
+  //      writer.println("""spark.driver.maxResultSize="2g"""")
+  //      writer.println("""spark.extraListeners=[]""")
+  //      writer.println("""spark.app.name="myApp"""")
+  //      writer.println("""spark.driver.memory="1g"""")
+  //      writer.println("""spark.driver.cores="10"""")
+  //      writer.println("""spark.master="local[*]"""")
+  //      writer.println("""spark.executor.memory="2g"""")
+  //      writer.println("""spark.local.dir="/tmp/"""")
+  //      writer.println("""spark.network.timeout=45s""")
+  //      // unused configuration
+  //      writer.println("""akka.loggers = ["akka.event.Logging$DefaultLogger"]""")
+  //      writer.close()
+  //
+  //      implicit def productHint[T] = ProductHint[T](ConfigFieldMapping(CamelCase, CamelCase))
+  //      val configOrError = loadConfig[SparkRootConf](configFile)
+  //
+  //      val config = configOrError match {
+  //        case Left(f) => fail(f.toString)
+  //        case Right(c) => c
+  //      }
+  //
+  //      config.spark.executor.extraJavaOptions should be("")
+  //      config.spark.driver.maxResultSize should be("2g")
+  //      config.spark.extraListeners should be(Seq.empty[String])
+  //      config.spark.app.name should be("myApp")
+  //      config.spark.driver.memory should be("1g")
+  //      config.spark.driver.cores should be(10)
+  //      config.spark.master should be("local[*]")
+  //      config.spark.executor.memory should be("2g")
+  //      config.spark.local.dir should be("/tmp/")
+  //      config.spark.network.timeout should be(FiniteDuration(45, TimeUnit.SECONDS))
+  //    }
+  //  }
 }
