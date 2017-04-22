78f717eef9ab47e4a64f4c2a5f9d54fac3cf508a
Rui Gonçalves <ruippeixotog@gmail.com>
2017-04-22 22:34:30 +0100

    Improve CoproductHint specs

--- core/src/test/scala/pureconfig/CoproductHintSuite.scala
+++ core/src/test/scala/pureconfig/CoproductHintSuite.scala
@@ -1,7 +1,7 @@
 package pureconfig
 
 import com.typesafe.config.{ ConfigFactory, ConfigObject, ConfigValueFactory, ConfigValueType }
-import pureconfig.error.{ ConfigReaderException, ConfigReaderFailures, WrongType }
+import pureconfig.error._
 
 class CoproductHintSuite extends BaseSuite {
 
@@ -23,10 +23,18 @@ class CoproductHintSuite extends BaseSuite {
       conf.asInstanceOf[ConfigObject].get("which-animal") shouldEqual ConfigValueFactory.fromAnyRef("Dog")
     }
 
+    it should "fail to read values that are not objects when using a FieldCoproductHint" in {
+      val conf = ConfigValueFactory.fromAnyRef("Dog")
+      ConfigConvert[AnimalConfig].from(conf) shouldEqual Left(ConfigReaderFailures(
+        WrongType(ConfigValueType.STRING, Set(ConfigValueType.OBJECT), None, None)))
+    }
+
     it should "throw an exception when the hint field conflicts with a field of an option when using a FieldCoproductHint" in {
       implicit val hint = new FieldCoproductHint[AnimalConfig]("age")
       val cc = implicitly[ConfigConvert[AnimalConfig]]
-      a[ConfigReaderException[_]] should be thrownBy cc.to(DogConfig(2))
+
+      val ex = the[ConfigReaderException[_]] thrownBy cc.to(DogConfig(2))
+      ex.failures.toList shouldEqual List(CollidingKeys("age", "ConfigInt(2)", None))
     }
   }
 
@@ -44,11 +52,15 @@ class CoproductHintSuite extends BaseSuite {
     it should "read values as expected when using an EnumCoproductHint" in {
       val conf = ConfigValueFactory.fromAnyRef("rainy-blue")
       ConfigConvert[Color].from(conf) shouldEqual Right(RainyBlue)
+      val conf2 = ConfigValueFactory.fromAnyRef("sunny-yellow")
+      ConfigConvert[Color].from(conf2) shouldEqual Right(SunnyYellow)
     }
 
     it should "write values as expected when using an EnumCoproductHint" in {
-      val conf = ConfigConvert[Color].to(SunnyYellow)
-      conf shouldEqual ConfigValueFactory.fromAnyRef("sunny-yellow")
+      val conf = ConfigConvert[Color].to(RainyBlue)
+      conf shouldEqual ConfigValueFactory.fromAnyRef("rainy-blue")
+      val conf2 = ConfigConvert[Color].to(SunnyYellow)
+      conf2 shouldEqual ConfigValueFactory.fromAnyRef("sunny-yellow")
     }
 
     it should "fail to read values that are not case objects when using an EnumCoproductHint" in {
@@ -58,7 +70,8 @@ class CoproductHintSuite extends BaseSuite {
     }
 
     it should "fail to write values that are not case objects when using an EnumCoproductHint" in {
-      a[ConfigReaderException[_]] should be thrownBy ConfigConvert[AnimalConfig].to(DogConfig(2))
+      val ex = the[ConfigReaderException[_]] thrownBy ConfigConvert[AnimalConfig].to(DogConfig(2))
+      ex.failures.toList shouldEqual List(NonEmptyObjectFound("DogConfig", None, None))
     }
   }
 
