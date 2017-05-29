7f17b11e0d51bd730fb03f2d60242c675dc83dfa
Mario Pastorelli <pastorelli.mario@gmail.com>
2017-05-29 21:59:03 +0200

    add test for value classes with private constructor

--- core/src/test/scala/pureconfig/ValueClassSuite.scala
+++ core/src/test/scala/pureconfig/ValueClassSuite.scala
@@ -1,8 +1,12 @@
 package pureconfig
 
+import shapeless.test.illTyped
+
 final class IntWrapper(val inner: Int) extends AnyVal {
   override def toString: String = s"IntWrapper($inner)"
 }
+
+final class PrivateFloatValue private (val value: Float) extends AnyVal
 
 class ValueClassSuite extends BaseSuite {
 
@@ -10,4 +14,8 @@ class ValueClassSuite extends BaseSuite {
 
   checkRead[IntWrapper](new IntWrapper(1) -> ConfigWriter.forPrimitive[Int].to(1))
   checkWrite[IntWrapper](new IntWrapper(1) -> ConfigWriter.forPrimitive[Int].to(1))
+
+  "ConfigReader[PrivateFloatValue]" should "not be derivable because the constructor is private" in {
+    illTyped("pureconfig.ConfigReader[PrivateFloatValue]")
+  }
 }
