3e0ed0cc17f172cbd99c9c8ff179d6eb4ab70072
Mario Pastorelli <pastorelli.mario@gmail.com>
2017-05-29 22:32:27 +0200

    add example of value class instance override

--- core/src/test/scala/pureconfig/ValueClassSuite.scala
+++ core/src/test/scala/pureconfig/ValueClassSuite.scala
@@ -8,6 +8,26 @@ final class IntWrapper(val inner: Int) extends AnyVal {
 
 final class PrivateFloatValue private (val value: Float) extends AnyVal
 
+final class GenericValue[T] private (val t: T) extends AnyVal {
+  override def toString: String = "GenericValue(" + t.toString + ")"
+}
+
+object GenericValue {
+  def from[T](t: T): GenericValue[T] = new GenericValue[T](t)
+}
+
+trait Read[T] {
+  def read(str: String): T
+}
+
+object Read {
+  def apply[T](implicit readT: Read[T]): Read[T] = readT
+
+  implicit val badReadString = new Read[String] {
+    override def read(str: String): String = ""
+  }
+}
+
 class ValueClassSuite extends BaseSuite {
 
   behavior of "ConfigConvert for Value Classes"
@@ -17,5 +37,17 @@ class ValueClassSuite extends BaseSuite {
 
   "ConfigReader[PrivateFloatValue]" should "not be derivable because the constructor is private" in {
     illTyped("pureconfig.ConfigReader[PrivateFloatValue]")
+  }
+
+  {
+    implicit def genericValueReader[T](implicit readT: Read[T]): ConfigReader[GenericValue[T]] =
+      ConfigReader.fromString(s => _ => Right(GenericValue.from(readT.read(s))))
+
+    "ConfigReader" should " should be able to override value classes ConfigReader" in {
+      val reader = ConfigReader[GenericValue[String]]
+      val expected = Right(GenericValue.from(""))
+      val configValue = ConfigWriter.forPrimitive[String].to("foobar")
+      reader.from(configValue) shouldEqual expected
+    }
   }
 }
