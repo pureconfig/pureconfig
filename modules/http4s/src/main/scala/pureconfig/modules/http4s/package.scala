00f1da7980e3a49de1fa1d17671ce349f198a514
JCranky <paulo.siqueira@zalando.de>
2018-03-05 18:05:49 +0100

    fixup - make the code friends with 2.11

--- modules/http4s/src/main/scala/pureconfig/modules/http4s/package.scala
+++ modules/http4s/src/main/scala/pureconfig/modules/http4s/package.scala
@@ -2,9 +2,13 @@ package pureconfig.modules
 
 import org.http4s.Uri
 import pureconfig.ConfigReader
+import pureconfig.error.CannotConvert
 
 package object http4s {
 
   implicit val uriReader: ConfigReader[Uri] =
-    ConfigReader.fromStringTry(Uri.fromString(_).toTry)
+    ConfigReader.fromString(str =>
+      Uri.fromString(str).fold(
+        err => Left(CannotConvert(str, "Uri", err.getMessage())),
+        uri => Right(uri)))
 }
