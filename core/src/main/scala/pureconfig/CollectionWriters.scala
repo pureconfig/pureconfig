db39f88c7135c8ccb12e0dc04ad1c331324d8bcf
Rui Gonçalves <ruippeixotog@gmail.com>
2018-09-30 23:03:39 +0100

    Correct typos

--- core/src/main/scala/pureconfig/CollectionWriters.scala
+++ core/src/main/scala/pureconfig/CollectionWriters.scala
@@ -9,7 +9,7 @@ import com.typesafe.config._
  * A trait signaling that a `ConfigWriter` can write missing (undefined) values.
  *
  * `ConfigWriter`s always produce a valid `ConfigValue` with their `to` method. This trait adds an extra `toOpt`
- * method that parent writers and use in order to decide whether or not they should write a value using this writer.
+ * method that parent writers can use in order to decide whether or not they should write a value using this writer.
  */
 trait WritesMissingKeys[A] { this: ConfigWriter[A] =>
   def toOpt(a: A): Option[ConfigValue]
