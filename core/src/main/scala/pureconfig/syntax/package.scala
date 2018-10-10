dceb1f00c7dc33f96e79399a6ef8c0f517a4867c
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-10-10 13:12:07 +0100

    Use Derivation in toConfig

--- core/src/main/scala/pureconfig/syntax/package.scala
+++ core/src/main/scala/pureconfig/syntax/package.scala
@@ -6,8 +6,8 @@ import pureconfig.error.{ ConfigReaderException, ConfigReaderFailures }
 import scala.reflect.ClassTag
 
 package object syntax {
-  implicit class PimpedAny[T](val any: T) extends AnyVal {
-    def toConfig(implicit writer: ConfigWriter[T]): ConfigValue = writer.to(any)
+  implicit class RichAny[T](val any: T) extends AnyVal {
+    def toConfig(implicit writer: Derivation[ConfigWriter[T]]): ConfigValue = writer.value.to(any)
   }
 
   private def getResultOrThrow[Config](failuresOrResult: Either[ConfigReaderFailures, Config])(implicit ct: ClassTag[Config]): Config = {
@@ -17,17 +17,17 @@ package object syntax {
     }
   }
 
-  implicit class PimpedConfigValue(val conf: ConfigValue) extends AnyVal {
+  implicit class RichConfigValue(val conf: ConfigValue) extends AnyVal {
     def to[T](implicit reader: Derivation[ConfigReader[T]]): Either[ConfigReaderFailures, T] = reader.value.from(conf)
     def toOrThrow[T](implicit reader: Derivation[ConfigReader[T]], cl: ClassTag[T]): T = getResultOrThrow(reader.value.from(conf))(cl)
   }
 
-  implicit class PimpedConfigCursor(val cur: ConfigCursor) extends AnyVal {
+  implicit class RichConfigCursor(val cur: ConfigCursor) extends AnyVal {
     def to[T](implicit reader: Derivation[ConfigReader[T]]): Either[ConfigReaderFailures, T] = reader.value.from(cur)
     def toOrThrow[T](implicit reader: Derivation[ConfigReader[T]], cl: ClassTag[T]): T = getResultOrThrow(reader.value.from(cur))(cl)
   }
 
-  implicit class PimpedConfig(val conf: TypesafeConfig) extends AnyVal {
+  implicit class RichConfig(val conf: TypesafeConfig) extends AnyVal {
     def to[T](implicit reader: Derivation[ConfigReader[T]]): Either[ConfigReaderFailures, T] = conf.root().to[T]
     def toOrThrow[T](implicit reader: Derivation[ConfigReader[T]], cl: ClassTag[T]): T = getResultOrThrow(conf.root().to[T])(cl)
   }
