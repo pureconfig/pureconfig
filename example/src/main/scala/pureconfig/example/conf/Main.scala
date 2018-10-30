535b7a746e8d3df7a87a15f903bfdf7184e71c72
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-10-30 03:23:02 +0000

    Fix compilation of example project

--- example/src/main/scala/pureconfig/example/conf/Main.scala
+++ example/src/main/scala/pureconfig/example/conf/Main.scala
@@ -10,6 +10,7 @@ import java.nio.file.Path
 
 import pureconfig._
 import pureconfig.example.conf._
+import pureconfig.generic.auto._
 
 /*
 This is an example of configuration for our directory watcher. The configuration needs:
