51123c08d52b157c9c3926d3886805ae75fa2897
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2019-04-13 09:46:59 +0100

    Replace reader/writer by ConfigConvert

--- modules/cron4s/src/main/scala/pureconfig/module/cron4s/package.scala
+++ modules/cron4s/src/main/scala/pureconfig/module/cron4s/package.scala
@@ -2,17 +2,15 @@ package pureconfig.module
 
 import _root_.cron4s.expr.CronExpr
 import _root_.cron4s.Cron
-import pureconfig.{ ConfigReader, ConfigWriter }
+import pureconfig.ConfigConvert
 import pureconfig.error.CannotConvert
 
 package object cron4s {
 
-  implicit val cronExprConfigReader: ConfigReader[CronExpr] =
-    ConfigReader.fromString(str =>
-      Cron.parse(str).fold(
+  implicit val cronExprConfigConvert: ConfigConvert[CronExpr] =
+    ConfigConvert.viaNonEmptyString(
+      str => Cron.parse(str).fold(
         err => Left(CannotConvert(str, "CronExpr", err.getMessage)),
-        cronExpr => Right(cronExpr)))
+        expr => Right(expr)), _.toString)
 
-  implicit val cronExprConfigWriter: ConfigWriter[CronExpr] =
-    ConfigWriter[String].contramap(_.toString)
 }
