e2a463ab6a6abfd28e1ef9b3c231269dc1ecbf74
Rui Gonçalves <ruippeixotog@gmail.com>
2019-04-28 18:45:19 -0700

    Add Scaladoc to FactoryCompat

--- core/src/main/scala-2.13.0-RC1/pureconfig/FactoryCompat.scala
+++ core/src/main/scala-2.13.0-RC1/pureconfig/FactoryCompat.scala
@@ -3,6 +3,13 @@ package pureconfig
 import scala.collection.Factory
 import scala.collection.mutable
 
+/**
+  * A compatibility layer for creating `CanBuildFrom`-like generic methods that work both on Scala 2.13 and pre-2.13
+  * versions.
+  *
+  * @tparam A the type of elements that get added to the builder
+  * @tparam C the type of collection that it produces
+  */
 trait FactoryCompat[-A, +C] {
   def newBuilder(): mutable.Builder[A, C]
 }
