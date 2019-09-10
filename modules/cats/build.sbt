29807fa97f180b0c8ec286a76913abef090ee14d
Joao Azevedo <joao.c.azevedo@gmail.com>
2019-09-11 00:12:51 +0100

    Add Eq instances for ConfigReader and ConfigWriter that don't depend on ExhaustiveCheck

--- modules/cats/build.sbt
+++ modules/cats/build.sbt
@@ -3,8 +3,9 @@ name := "pureconfig-cats"
 crossScalaVersions ~= { _.filterNot(_.startsWith("2.13")) }
 
 libraryDependencies ++= Seq(
-  "org.typelevel" %% "cats-core" % "2.0.0",
-  "org.typelevel" %% "cats-laws" % "2.0.0" % "test")
+  "org.typelevel" %% "cats-core"  % "2.0.0",
+  "org.typelevel" %% "cats-laws"  % "2.0.0"  % "test",
+  "org.typelevel" %% "discipline" % "0.11.1" % "test")
 
 developers := List(
   Developer("derekmorr", "Derek Morr", "morr.derek@gmail.com", url("https://github.com/derekmorr")),
--- modules/cats/src/test/scala/pureconfig/module/cats/eq/package.scala
+++ modules/cats/src/test/scala/pureconfig/module/cats/eq/package.scala
@@ -3,7 +3,6 @@ package pureconfig.module.cats
 import cats.Eq
 import cats.instances.either._
 import cats.instances.tuple._
-import cats.laws.discipline.eq._
 import com.typesafe.config.ConfigValue
 import org.scalacheck.Arbitrary
 import pureconfig._
@@ -12,11 +11,33 @@ import pureconfig.module.cats.instances._
 
 package object eq {
 
-  implicit def configReaderEq[A: Eq]: Eq[ConfigReader[A]] =
-    Eq.by[ConfigReader[A], ConfigValue => ConfigReader.Result[A]](_.from)
+  implicit def configReaderEq[A: Eq](implicit arbCV: Arbitrary[ConfigValue]): Eq[ConfigReader[A]] = new Eq[ConfigReader[A]] {
+    val nSamples = 50
+    val resultEq = Eq[ConfigReader.Result[A]]
 
-  implicit def configWriterEq[A: Arbitrary]: Eq[ConfigWriter[A]] =
-    Eq.by[ConfigWriter[A], A => ConfigValue](_.to)
+    def eqv(x: ConfigReader[A], y: ConfigReader[A]): Boolean = {
+      val samples = List.fill(nSamples)(arbCV.arbitrary.sample).collect {
+        case Some(a) => a
+        case None => sys.error("Could not generate arbitrary values to compare two ConfigReaders")
+      }
+
+      samples.forall(s => resultEq.eqv(x.from(s), y.from(s)))
+    }
+  }
+
+  implicit def configWriterEq[A](implicit arbA: Arbitrary[A]): Eq[ConfigWriter[A]] = new Eq[ConfigWriter[A]] {
+    val nSamples = 50
+    val resultEq = Eq[ConfigValue]
+
+    def eqv(x: ConfigWriter[A], y: ConfigWriter[A]): Boolean = {
+      val samples = List.fill(nSamples)(arbA.arbitrary.sample).collect {
+        case Some(a) => a
+        case None => sys.error("Could not generate arbitrary values to compare two ConfigWriters")
+      }
+
+      samples.forall(s => resultEq.eqv(x.to(s), y.to(s)))
+    }
+  }
 
   implicit def configConvertEq[A: Eq: Arbitrary]: Eq[ConfigConvert[A]] =
     Eq.by[ConfigConvert[A], (ConfigReader[A], ConfigWriter[A])] { cc => (cc, cc) }
