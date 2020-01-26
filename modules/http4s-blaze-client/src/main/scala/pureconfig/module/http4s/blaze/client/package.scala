7a45269e66721c94195178012d28a511a35af3ad
Ondra Pelech <ondra.pelech@gmail.com>
2020-01-27 00:23:50 +0100

    tmp2

--- modules/http4s-blaze-client/src/main/scala/pureconfig/module/http4s/blaze/client/package.scala
+++ modules/http4s-blaze-client/src/main/scala/pureconfig/module/http4s/blaze/client/package.scala
@@ -1,11 +1,15 @@
 package pureconfig.module.http4s.blaze
 
+import cats.data.NonEmptyChain
+import cats.effect.ConcurrentEffect
+import cats.implicits._
+import javax.net.ssl.SSLContext
 import org.http4s.Uri
 import org.http4s.client.blaze.BlazeClientBuilder
-import pureconfig.error.CannotConvert
-import pureconfig.{ConfigReader, ConfigWriter}
-import cats.implicits._
-import cats.data.NonEmptyChain
+import pureconfig.error.{CannotConvert, ConfigReaderFailures}
+import pureconfig.{ConfigCursor, ConfigReader, ConfigWriter}
+
+import scala.concurrent.ExecutionContext
 
 package object client {
 
@@ -23,35 +27,37 @@ package object client {
   implicit val uriWriter: ConfigWriter[Uri] =
     ConfigWriter[String].contramap(_.renderString)
 
-  implicit def blazeClientBuilderReader[F[_]]
-    : ConfigReader[BlazeClientBuilder[F]] =
-    ConfigReader.fromCursor[BlazeClientBuilder[F]] { cur =>
-      cur.asObjectCursor.flatMap { objCur =>
-        val x = objCur.map
-          .map {
-            case ("bufferSize", crs) =>
-              for {
-                bufferSize <- crs.asInt
-              } yield
-                ((b: BlazeClientBuilder[F]) => b.withBufferSize(bufferSize))
-          }
-          .map {
-            _.leftMap(crfs => NonEmptyChain(crfs.head, crfs.tail: _*)).toValidated
-          }
-          .toList
-        val y = x.sequence
-        ???
-      }
-//      for {
-//        objCur <- cur.asObjectCursor // 1
-//        nameCur <- objCur.map.map {
-//          case ("bufferSize", crs) =>
-//            for {
-//              bufferSize <- crs.asInt
-//            } yield ((b: BlazeClientBuilder[F]) => b.withBufferSize(bufferSize))
-//        } // 2
-//        name <- nameCur.asString // 3
-//      } yield new Person(firstNameOf(name), lastNamesOf(name))
+  implicit def blazeClientBuilderReader[F[_]: ConcurrentEffect]: ConfigReader[
+    (ExecutionContext, Option[SSLContext]) => BlazeClientBuilder[F]
+  ] = {
+    def withField(map: (String, ConfigCursor)) = map match {
+      case ("bufferSize", crs) =>
+        crs.asInt.map { bufferSize =>
+          Some((b: BlazeClientBuilder[F]) => b.withBufferSize(bufferSize))
+        }
+      case _ => Right(None)
     }
+    ConfigReader
+      .fromCursor[(ExecutionContext, Option[SSLContext]) => BlazeClientBuilder[
+        F
+      ]] { cur =>
+        for {
+          objCur <- cur.asObjectCursor
+          transformations <- objCur.map.toList
+            .map(withField)
+            .map {
+              _.leftMap(crfs => NonEmptyChain(crfs.head, crfs.tail: _*))
+            }
+            .sequence
+            .leftMap(nec => ConfigReaderFailures(nec.head, nec.tail.toList))
+            .map(_.flattenOption)
+          constructor = { (ec: ExecutionContext, ssl: Option[SSLContext]) =>
+            transformations.foldl(BlazeClientBuilder[F](ec, ssl)) {
+              case (b, f) => f(b)
+            }
+          }
+        } yield constructor
+      }
+  }
 
 }
