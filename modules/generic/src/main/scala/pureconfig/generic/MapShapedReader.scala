0fc5acee197692b8579fe7d957e7b0e8766ceda0
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-04-28 23:47:05 +0100

    Break long lines

--- modules/generic/src/main/scala/pureconfig/generic/MapShapedReader.scala
+++ modules/generic/src/main/scala/pureconfig/generic/MapShapedReader.scala
@@ -20,34 +20,36 @@ private[generic] trait MapShapedReader[Wrapped, Repr, DefaultRepr] {
 
 object MapShapedReader {
 
-  implicit def labelledHNilReader[Wrapped](implicit hint: ProductHint[Wrapped]): MapShapedReader[Wrapped, HNil, HNil] = new MapShapedReader[Wrapped, HNil, HNil] {
-
-    def from(cur: ConfigObjectCursor, default: HNil, usedFields: Set[String]): ConfigReader.Result[HNil] =
-      hint.bottom(cur, usedFields).fold[ConfigReader.Result[HNil]](Right(HNil))(Left.apply)
-  }
+  implicit def labelledHNilReader[Wrapped](
+    implicit
+    hint: ProductHint[Wrapped]): MapShapedReader[Wrapped, HNil, HNil] =
+    new MapShapedReader[Wrapped, HNil, HNil] {
+      def from(cur: ConfigObjectCursor, default: HNil, usedFields: Set[String]): ConfigReader.Result[HNil] =
+        hint.bottom(cur, usedFields).fold[ConfigReader.Result[HNil]](Right(HNil))(Left.apply)
+    }
 
   final implicit def labelledHConsReader[Wrapped, K <: Symbol, V, T <: HList, U <: HList](
     implicit
     key: Witness.Aux[K],
-    vFieldReader: Derivation[Lazy[ConfigReader[V]]],
+    vConfigReader: Derivation[Lazy[ConfigReader[V]]],
     tConfigReader: Lazy[MapShapedReader[Wrapped, T, U]],
-    hint: ProductHint[Wrapped]): MapShapedReader[Wrapped, FieldType[K, V] :: T, Option[V] :: U] = new MapShapedReader[Wrapped, FieldType[K, V] :: T, Option[V] :: U] {
-
-    def from(cur: ConfigObjectCursor, default: Option[V] :: U, usedFields: Set[String]): ConfigReader.Result[FieldType[K, V] :: T] = {
-      val fieldName = key.value.name
-      val fieldAction = hint.from(cur, fieldName)
-      lazy val reader = vFieldReader.value.value
-      lazy val keyNotFoundFailure = cur.failed[V](KeyNotFound.forKeys(fieldAction.field, cur.keys))
-      val headResult = (fieldAction, default.head) match {
-        case (UseOrDefault(cursor, _), Some(defaultValue)) if cursor.isUndefined =>
-          Right(defaultValue)
-        case (action, _) if reader.isInstanceOf[ReadsMissingKeys] || !action.cursor.isUndefined =>
-          reader.from(action.cursor)
-        case _ =>
-          keyNotFoundFailure
+    hint: ProductHint[Wrapped]): MapShapedReader[Wrapped, FieldType[K, V] :: T, Option[V] :: U] =
+    new MapShapedReader[Wrapped, FieldType[K, V] :: T, Option[V] :: U] {
+      def from(cur: ConfigObjectCursor, default: Option[V] :: U, usedFields: Set[String]): ConfigReader.Result[FieldType[K, V] :: T] = {
+        val fieldName = key.value.name
+        val fieldAction = hint.from(cur, fieldName)
+        lazy val reader = vConfigReader.value.value
+        lazy val keyNotFoundFailure = cur.failed[V](KeyNotFound.forKeys(fieldAction.field, cur.keys))
+        val headResult = (fieldAction, default.head) match {
+          case (UseOrDefault(cursor, _), Some(defaultValue)) if cursor.isUndefined =>
+            Right(defaultValue)
+          case (action, _) if reader.isInstanceOf[ReadsMissingKeys] || !action.cursor.isUndefined =>
+            reader.from(action.cursor)
+          case _ =>
+            keyNotFoundFailure
+        }
+        val tailResult = tConfigReader.value.from(cur, default.tail, usedFields + fieldAction.field)
+        ConfigReader.Result.zipWith(headResult, tailResult)((head, tail) => field[K](head) :: tail)
       }
-      val tailResult = tConfigReader.value.from(cur, default.tail, usedFields + fieldAction.field)
-      ConfigReader.Result.zipWith(headResult, tailResult)((head, tail) => field[K](head) :: tail)
     }
-  }
 }
--- modules/generic/src/main/scala/pureconfig/generic/MapShapedWriter.scala
+++ modules/generic/src/main/scala/pureconfig/generic/MapShapedWriter.scala
@@ -24,27 +24,27 @@ object MapShapedWriter {
   final implicit def labelledHConsWriter[Wrapped, K <: Symbol, V, T <: HList, U <: HList](
     implicit
     key: Witness.Aux[K],
-    vFieldWriter: Derivation[Lazy[ConfigWriter[V]]],
+    vConfigWriter: Derivation[Lazy[ConfigWriter[V]]],
     tConfigWriter: Lazy[MapShapedWriter[Wrapped, T]],
-    hint: ProductHint[Wrapped]): MapShapedWriter[Wrapped, FieldType[K, V] :: T] = new MapShapedWriter[Wrapped, FieldType[K, V] :: T] {
+    hint: ProductHint[Wrapped]): MapShapedWriter[Wrapped, FieldType[K, V] :: T] =
+    new MapShapedWriter[Wrapped, FieldType[K, V] :: T] {
+      override def to(t: FieldType[K, V] :: T): ConfigValue = {
+        val rem = tConfigWriter.value.to(t.tail)
+        val valueOpt = vConfigWriter.value.value match {
+          case tc: WritesMissingKeys[V @unchecked] =>
+            tc.toOpt(t.head)
+          case w =>
+            Some(w.to(t.head))
+        }
+        val kv = hint.to(valueOpt, key.value.name)
 
-    override def to(t: FieldType[K, V] :: T): ConfigValue = {
-      val rem = tConfigWriter.value.to(t.tail)
-      val valueOpt = vFieldWriter.value.value match {
-        case w: WritesMissingKeys[V @unchecked] =>
-          w.toOpt(t.head)
-        case w =>
-          Some(w.to(t.head))
-      }
-      val kv = hint.to(valueOpt, key.value.name)
-
-      // TODO check that all keys are unique
-      kv.fold(rem) {
-        case (k, v) =>
-          rem.asInstanceOf[ConfigObject].withValue(k, v)
+        // TODO check that all keys are unique
+        kv.fold(rem) {
+          case (k, v) =>
+            rem.asInstanceOf[ConfigObject].withValue(k, v)
+        }
       }
     }
-  }
 
   implicit def cNilWriter[Wrapped]: MapShapedWriter[Wrapped, CNil] = new MapShapedWriter[Wrapped, CNil] {
     override def to(t: CNil): ConfigValue =
@@ -58,7 +58,6 @@ object MapShapedWriter {
     vConfigWriter: Derivation[Lazy[ConfigWriter[V]]],
     tConfigWriter: Lazy[MapShapedWriter[Wrapped, T]]): MapShapedWriter[Wrapped, FieldType[Name, V] :+: T] =
     new MapShapedWriter[Wrapped, FieldType[Name, V] :+: T] {
-
       override def to(t: FieldType[Name, V] :+: T): ConfigValue = t match {
         case Inl(l) =>
           coproductHint.to(vConfigWriter.value.value.to(l), vName.value.name)
