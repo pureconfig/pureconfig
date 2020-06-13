73c5e07904c0c321be0a71681941980c30df4ef5
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-06-13 18:20:31 +0100

    Make `toList` on `ConfigReaderFailures` a lazy val

--- core/src/main/scala/pureconfig/error/ConfigReaderFailures.scala
+++ core/src/main/scala/pureconfig/error/ConfigReaderFailures.scala
@@ -10,7 +10,7 @@ import scala.collection.mutable
  */
 case class ConfigReaderFailures(head: ConfigReaderFailure, tail: ConfigReaderFailure*) {
 
-  def toList: List[ConfigReaderFailure] = head :: tail.toList
+  lazy val toList: List[ConfigReaderFailure] = head :: tail.toList
 
   def +:(failure: ConfigReaderFailure): ConfigReaderFailures =
     new ConfigReaderFailures(failure, this.toList: _*)
