e324a89663185d02213562a85e88d8600505f512
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-22 12:44:04 +0200

    Update scalatest to 3.2.2 (#815)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val shapeless               = "2.3.3"
     val typesafeConfig          = "1.4.0"
 
-    val scalaTest               = "3.2.1"
+    val scalaTest               = "3.2.2"
     val scalaTestPlusScalaCheck = "3.2.1.0"
 
     val scalaCheck              = "1.14.3"
