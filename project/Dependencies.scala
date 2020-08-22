ab780592b66d11ca93ef25c6efdd7117b8202e88
Scala Steward <me@scala-steward.org>
2020-08-22 08:03:09 +0200

    Update scalacheck-1-14 to 3.2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val typesafeConfig          = "1.4.0"
 
     val scalaTest               = "3.2.1"
-    val scalaTestPlusScalaCheck = "3.2.1.0"
+    val scalaTestPlusScalaCheck = "3.2.2.0"
 
     val scalaCheck              = "1.14.3"
     val scalaCheckShapeless     = "1.2.5"
