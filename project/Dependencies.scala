a13b61ce7713683d22b6b9e22def20c6bb415c89
Joao Azevedo <joao.c.azevedo@gmail.com>
2021-02-27 13:33:17 +0000

    Update Scala 2.13 to 2.13.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependencies {
 
   object Version {
-    val scala211 = "2.13.5"
+    val scala211 = "2.11.12"
     val scala212 = "2.12.12"
-    val scala213 = "2.13.3"
+    val scala213 = "2.13.5"
     val scala30 = "3.0.0-M3"
 
     val shapeless = "2.3.3"
