74fa587ccbe633976b593f5d8183aa9eb83e7789
Rui Gonçalves <ruippeixotog@gmail.com>
2021-02-28 01:29:17 +0000

    Bump Scala 3 to version 3.0.0-RC1 (#959)
    * Bump Scala 3 to version 3.0.0-RC1
    
    * Update Scala 3 version in GitHub Actions
    
    * Bump scalatest version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,13 +5,13 @@ object Dependencies {
   object Version {
     val scala212 = "2.12.12"
     val scala213 = "2.13.5"
-    val scala30 = "3.0.0-M3"
+    val scala30 = "3.0.0-RC1"
 
     val shapeless = "2.3.3"
     val typesafeConfig = "1.4.1"
 
-    val scalaTest = "3.2.3"
-    val scalaTestPlusScalaCheck = "3.2.2.0"
+    val scalaTest = "3.2.5"
+    val scalaTestPlusScalaCheck = "3.2.5.0"
 
     val scalaCheck = "1.15.2"
     val scalaCheckShapeless = "1.2.5"
@@ -22,7 +22,7 @@ object Dependencies {
 
   // testing libraries
   val scalaTest = "org.scalatest" %% "scalatest" % Version.scalaTest
-  val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-14" % Version.scalaTestPlusScalaCheck
+  val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-15" % Version.scalaTestPlusScalaCheck
   val scalaCheck = "org.scalacheck" %% "scalacheck" % Version.scalaCheck
   val scalaCheckShapeless =
     "com.github.alexarchambault" %% s"scalacheck-shapeless_1.14" % Version.scalaCheckShapeless
