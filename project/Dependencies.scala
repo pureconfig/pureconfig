d0e87eec96bc68b504513d31ff6d25c44f82b138
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-03-08 11:24:58 +0100

    Update scalacheck-1-15 to 3.2.6.0 (#973)
    Co-authored-by: Rui Gonçalves <ruippeixotog@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val typesafeConfig = "1.4.1"
 
     val scalaTest = "3.2.6"
-    val scalaTestPlusScalaCheck = "3.2.5.0"
+    val scalaTestPlusScalaCheck = "3.2.6.0"
 
     val scalaCheck = "1.15.3"
     val scalaCheckShapeless = "1.2.5"
