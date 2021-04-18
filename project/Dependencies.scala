c7eca35e980f57ebfb719c8d735e5f1bd076855f
Joao Azevedo <joao.c.azevedo@gmail.com>
2021-04-18 20:59:55 +0100

    Update Scala 3 to 3.0.0-RC2 (#1016)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,12 +5,12 @@ object Dependencies {
   object Version {
     val scala212 = "2.12.12"
     val scala213 = "2.13.5"
-    val scala30 = "3.0.0-RC1"
+    val scala30 = "3.0.0-RC2"
 
     val shapeless = "2.3.4"
     val typesafeConfig = "1.4.1"
 
-    val scalaTest = "3.2.6"
+    val scalaTest = "3.2.7"
     val scalaTestPlusScalaCheck = "3.2.7.0"
 
     val scalaCheck = "1.15.3"
