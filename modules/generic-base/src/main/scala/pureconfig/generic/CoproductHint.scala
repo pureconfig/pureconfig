e4cb9f5557963f87169055f0471488861e68e4f9
Scala Steward <me@scala-steward.org>
2021-04-29 12:40:52 +0200

    Reformat with scalafmt 3.0.0-RC2

--- modules/generic-base/src/main/scala/pureconfig/generic/CoproductHint.scala
+++ modules/generic-base/src/main/scala/pureconfig/generic/CoproductHint.scala
@@ -14,7 +14,8 @@ import pureconfig.syntax._
 
 /** A trait that can be implemented to disambiguate between the different options of a coproduct or sealed family.
   *
-  * @tparam A the type of the coproduct or sealed family for which this hint applies
+  * @tparam A
+  *   the type of the coproduct or sealed family for which this hint applies
   */
 trait CoproductHint[A] {
 
@@ -25,19 +26,25 @@ trait CoproductHint[A] {
     *
     * This method can return a `Left` if the hint fails to produce a valid [[CoproductHint.Action]].
     *
-    * @param cursor a `ConfigCursor` at the sealed family option
-    * @param options the names of the coproduct options for the given type
-    * @return a `ConfigReader.Result` of [[CoproductHint.Action]] as defined above.
+    * @param cursor
+    *   a `ConfigCursor` at the sealed family option
+    * @param options
+    *   the names of the coproduct options for the given type
+    * @return
+    *   a `ConfigReader.Result` of [[CoproductHint.Action]] as defined above.
     */
   def from(cursor: ConfigCursor, options: Seq[String]): ConfigReader.Result[CoproductHint.Action]
 
   /** Given the `ConfigValue` for a specific class or coproduct option, encode disambiguation information and return a
     * config for the sealed family or coproduct.
     *
-    * @param value the `ConfigValue` of the class or coproduct option
-    * @param name the name of the class or coproduct option
-    * @return the config for the sealed family or coproduct wrapped in a `Right`, or a `Left` with the failure if some
-    *         error occurred.
+    * @param value
+    *   the `ConfigValue` of the class or coproduct option
+    * @param name
+    *   the name of the class or coproduct option
+    * @return
+    *   the config for the sealed family or coproduct wrapped in a `Right`, or a `Left` with the failure if some error
+    *   occurred.
     */
   def to(value: ConfigValue, name: String): ConfigValue
 }
@@ -54,8 +61,10 @@ class FieldCoproductHint[A](key: String) extends CoproductHint[A] {
 
   /** Returns the field value for a class or coproduct option name.
     *
-    * @param name the name of the class or coproduct option
-    * @return the field value associated with the given class or coproduct option name.
+    * @param name
+    *   the name of the class or coproduct option
+    * @return
+    *   the field value associated with the given class or coproduct option name.
     */
   protected def fieldValue(name: String): String = FieldCoproductHint.defaultMapping(name)
 
@@ -89,8 +98,8 @@ object FieldCoproductHint {
   val defaultMapping: String => String = ConfigFieldMapping(PascalCase, KebabCase)
 }
 
-/** Hint where all coproduct options are tried in order. `from` will choose the first option able to deserialize
-  * the config without errors, while `to` will write the config as is, with no disambiguation information.
+/** Hint where all coproduct options are tried in order. `from` will choose the first option able to deserialize the
+  * config without errors, while `to` will write the config as is, with no disambiguation information.
   */
 class FirstSuccessCoproductHint[A] extends CoproductHint[A] {
   def from(cursor: ConfigCursor, options: Seq[String]): ConfigReader.Result[CoproductHint.Action] =
@@ -121,18 +130,24 @@ object CoproductHint {
 
   /** An action to only use the provided `ConfigCursor` and not try other options.
     *
-    * @param cursor the `ConfigCursor` to use when reading the coproduct option
-    * @param option the coproduct option to consider when reading from the provider cursor
+    * @param cursor
+    *   the `ConfigCursor` to use when reading the coproduct option
+    * @param option
+    *   the coproduct option to consider when reading from the provider cursor
     */
-  case class Use(cursor: ConfigCursor, option: String) extends Action
+  case class Use(cursor: ConfigCursor, option: String)
+      extends Action
 
-  /** An action to attempt to use the provided coproduct options, in the specified order, stopping at the first one that
-    * reads successfully.
-    *
-    * @param cursor the `ConfigCursor` to use when reading the coproduct option
-    * @param options the coproduct options to attempt reading, in order
-    * @param combineFailures the function to combine all failures in case all attempts to read fail
-    */
+      /** An action to attempt to use the provided coproduct options, in the specified order, stopping at the first one
+        * that reads successfully.
+        *
+        * @param cursor
+        *   the `ConfigCursor` to use when reading the coproduct option
+        * @param options
+        *   the coproduct options to attempt reading, in order
+        * @param combineFailures
+        *   the function to combine all failures in case all attempts to read fail
+        */
   case class Attempt(
       cursor: ConfigCursor,
       options: Seq[String],
