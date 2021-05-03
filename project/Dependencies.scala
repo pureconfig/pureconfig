746a1ef6bb7b37e2dd1edf8267641c57f61cf325
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-03 22:13:44 +0200

    Update scalacheck to 1.15.4 (#1034)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val scalaTest = "3.2.7"
     val scalaTestPlusScalaCheck = "3.2.8.0"
 
-    val scalaCheck = "1.15.3"
+    val scalaCheck = "1.15.4"
     val scalaCheckShapeless = "1.2.5"
   }
 
