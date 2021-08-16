c9077094f9c77c9f83da732cfcfae02e336b935a
Ang Hao Yang <yan9za1@gmail.com>
2021-08-16 19:18:32 +0800

    inline field names

--- modules/spark/src/main/scala/pureconfig/module/spark/sql/package.scala
+++ modules/spark/src/main/scala/pureconfig/module/spark/sql/package.scala
@@ -18,18 +18,19 @@ package object sql {
   implicit val metadataConvert: ConfigConvert[Metadata] =
     ConfigConvert.viaNonEmptyStringTry[Metadata](s => Try(Metadata.fromJson(s)), _.json)
 
-  private val (key0, key1, key2, key3) = ("name", "data-type", "nullable", "metadata")
-
-  implicit val structFieldReader: ConfigReader[StructField] = ConfigReader.forProduct4(key0, key1, key2, key3) {
-    (name: String, dataType: DataType, maybeNullable: Option[Boolean], maybeMetadata: Option[Metadata]) =>
-      (maybeNullable, maybeMetadata) match {
-        case (Some(n), Some(m)) => StructField(name, dataType, n, m)
-        case (Some(n), _) => StructField(name, dataType, n)
-        case (_, Some(m)) => StructField(name, dataType, metadata = m)
-        case _ => StructField(name, dataType)
-      }
-  }
+  implicit val structFieldReader: ConfigReader[StructField] =
+    ConfigReader.forProduct4("name", "data-type", "nullable", "metadata") {
+      (name: String, dataType: DataType, maybeNullable: Option[Boolean], maybeMetadata: Option[Metadata]) =>
+        (maybeNullable, maybeMetadata) match {
+          case (Some(n), Some(m)) => StructField(name, dataType, n, m)
+          case (Some(n), _) => StructField(name, dataType, n)
+          case (_, Some(m)) => StructField(name, dataType, metadata = m)
+          case _ => StructField(name, dataType)
+        }
+    }
 
   implicit val structFieldWriter: ConfigWriter[StructField] =
-    ConfigWriter.forProduct4(key0, key1, key2, key3) { case StructField(name, dt, n, m) => (name, dt, n, m) }
+    ConfigWriter.forProduct4("name", "data-type", "nullable", "metadata") { case StructField(name, dt, n, m) =>
+      (name, dt, n, m)
+    }
 }
