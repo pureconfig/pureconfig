089cc134011d7a5f3c959a17f67e47af7059eb57
Scala Steward <me@scala-steward.org>
2021-09-18 02:43:58 +0200

    Update scalacheck-1-15 to 3.2.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val typesafeConfig = "1.4.1"
 
     val scalaTest = "3.2.9"
-    val scalaTestPlusScalaCheck = "3.2.9.0"
+    val scalaTestPlusScalaCheck = "3.2.10.0"
 
     val scalaCheck = "1.15.4"
   }
