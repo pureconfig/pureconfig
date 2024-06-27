cf1ff218c824f06feae630a708a70f8e6858440d
Grzegorz Bielski <pesiok@gmail.com>
2024-06-27 20:13:32 +0200

    Simplify exposed derivation interface

--- modules/generic-scala3/src/main/scala/pureconfig/generic/scala3/HintsAwareConfigWriterDerivation.scala
+++ modules/generic-scala3/src/main/scala/pureconfig/generic/scala3/HintsAwareConfigWriterDerivation.scala
@@ -10,22 +10,28 @@ import derivation.Utils._
 trait HintsAwareConfigWriterDerivation
     extends HintsAwareCoproductConfigWriterDerivation,
       HintsAwareProductConfigWriterDerivation {
-  inline def deriveWriter[A <: AnyVal]: ConfigWriter[A] = AnyValDerivationMacros.unsafeDeriveAnyValWriter[A](this)
-
-  inline def deriveWriter[A](using m: Mirror.Of[A]): ConfigWriter[A] =
-    inline m match {
-      case pm: Mirror.ProductOf[A] => deriveProductWriter[A](using pm, summonInline[ProductHint[A]])
-      case sm: Mirror.SumOf[A] => deriveSumWriter[A](using sm, summonInline[CoproductHint[A]])
+  inline def deriveWriter[A]: ConfigWriter[A] =
+    summonFrom {
+      case given Mirror.Of[A] => deriveWriterWithMirror[A]
+      case _ => deriveAnyValOrFail[A]
     }
 
   private[scala3] inline def summonConfigWriter[A]: ConfigWriter[A] =
     summonFrom {
       case writer: ConfigWriter[A] => writer
-      case given Mirror.Of[A] => deriveWriter[A]
-      case _ =>
-        inline if (AnyValDerivationMacros.isAnyVal[A]) AnyValDerivationMacros.unsafeDeriveAnyValWriter[A](this)
-        else error("Cannot derive ConfigWriter for: " + typeName[A])
+      case given Mirror.Of[A] => deriveWriterWithMirror[A]
+      case _ => deriveAnyValOrFail[A]
     }
+
+  private inline def deriveWriterWithMirror[A](using m: Mirror.Of[A]): ConfigWriter[A] =
+    inline m match {
+      case pm: Mirror.ProductOf[A] => deriveProductWriter[A](using pm, summonInline[ProductHint[A]])
+      case sm: Mirror.SumOf[A] => deriveSumWriter[A](using sm, summonInline[CoproductHint[A]])
+    }
+
+  private inline def deriveAnyValOrFail[A]: ConfigWriter[A] =
+    inline if (AnyValDerivationMacros.isAnyVal[A]) AnyValDerivationMacros.unsafeDeriveAnyValWriter[A](this)
+    else error("Cannot derive ConfigWriter for: " + typeName[A])
 }
 
 object HintsAwareConfigWriterDerivation extends HintsAwareConfigWriterDerivation
