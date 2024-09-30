cc2b875ca99c7af10a2fbd736cd0fc07d15813fd
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-10-01 00:22:19 +0200

    Update scala-library, scala-reflect to 2.13.15 (#1719)
    * Update scala-library, scala-reflect to 2.13.15
    
    * Update sbt-scoverage to v2.2.1
    
    ---------
    
    Co-authored-by: Rui Gonçalves <ruippeixotog@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   object Version {
     val scala212 = "2.12.20"
-    val scala213 = "2.13.14"
+    val scala213 = "2.13.15"
     val scala3 = "3.3.3"
 
     val scalaTest = "3.2.19"
