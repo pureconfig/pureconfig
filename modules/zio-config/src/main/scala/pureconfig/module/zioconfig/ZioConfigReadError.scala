ac4c62e53908f68fcafdd1e69a3f1757203d6f75
Rui Gonçalves <ruippeixotog@gmail.com>
2025-04-25 13:36:06 +0100

    Update to zio-config 4.x APIs

--- modules/zio-config/src/main/scala/pureconfig/module/zioconfig/ZioConfigReadError.scala
+++ modules/zio-config/src/main/scala/pureconfig/module/zioconfig/ZioConfigReadError.scala
@@ -1,9 +1,9 @@
 package pureconfig.module.zioconfig
 
-import zio.config.ReadError
+import zio.Config
 
 import pureconfig.error.FailureReason
 
-final case class ZioConfigReadError[A](re: ReadError[A]) extends FailureReason {
+final case class ZioConfigReadError[A](re: Config.Error) extends FailureReason {
   override def description: String = s"ZioConfigReadError: $re"
 }
