f6adfff7453ab561fe30140357e40fba023734a2
Oleg Nizhnikov <odomontois@gmail.com>
2025-09-09 15:27:29 +0200

    Return tests for optional embedding

--- modules/generic-scala3/src/main/scala/pureconfig/generic/scala3/DerivationFlow.scala
+++ modules/generic-scala3/src/main/scala/pureconfig/generic/scala3/DerivationFlow.scala
@@ -1,14 +1,14 @@
 package pureconfig.generic.scala3
 
 /** Context of macro derivation.
- *
- * An instance of this class is intended to be passed as an inline parameter, with the compiler knowing the precise values of all
- * fields
- *
- * @param allowAutoSum
- * If true, auto derivation of enums and sealed traits is currently allowed, because we are deriving an instance for a
- *   top-level union
- */
+  *
+  * An instance of this class is intended to be passed as an inline parameter, with the compiler knowing the precise
+  * values of all fields
+  *
+  * @param allowAutoSum
+  *   If true, auto derivation of enums and sealed traits is currently allowed, because we are deriving an instance for
+  *   a top-level union
+  */
 class DerivationFlow(val allowAutoSum: Boolean)
 
 object DerivationFlow {
--- modules/generic-scala3/src/test/scala/pureconfig/generic/SemiautoDerivationSuite.scala
+++ modules/generic-scala3/src/test/scala/pureconfig/generic/SemiautoDerivationSuite.scala
@@ -28,10 +28,40 @@ class SemiautoDerivationSuite extends BaseSuite {
   def allKeys(configObject: ConfigObject): Set[String] =
     configObject.toConfig().entrySet().asScala.flatMap(_.getKey.split('.')).toSet
 
+  it should "derive mostly the same reader instance if inner instance is in scope" in {
+    given ConfigReader[ConfWithCamelCaseInner] = deriveReader
+    given ConfigReader[ConfWithCamelCase] = deriveReader
+
+    val conf = ConfigFactory.parseString("""{
+      camel-case-int = 1
+      camel-case-string = "bar"
+      camel-case-conf {
+        this-is-an-int = 3
+        this-is-another-int = 10
+      }
+    }""")
+
+    conf.to[ConfWithCamelCase] shouldBe Right(ConfWithCamelCase(1, "bar", Some(ConfWithCamelCaseInner(3, 10))))
+  }
+
   it should "throw an error during reader derivation if inner instance is missing" in {
     val errors = typeCheckErrors("""given ConfigReader[ConfWithCamelCase] = deriveReader""").map(_.message)
 
     atLeast(1, errors) should (startWith("Cannot derive ConfigReader for") and include("ConfWithCamelCaseInner"))
+  }
+
+  it should "derive mostly the same writer instance if inner instance is in scope" in {
+    given ConfigWriter[ConfWithCamelCaseInner] = deriveWriter
+    given ConfigWriter[ConfWithCamelCase] = deriveWriter
+
+    val conf = confWithCamelCase.toConfig.asInstanceOf[ConfigObject]
+    allKeys(conf) should contain theSameElementsAs Seq(
+      "camel-case-int",
+      "camel-case-string",
+      "camel-case-conf",
+      "this-is-an-int",
+      "this-is-another-int"
+    )
   }
 
   it should "throw an error during writer derivation if inner instance is missing" in {
