e9cc0ca7806ff9af2453c21ead912da2bd95031b
afonsoafonsoafonso <afonso.mendonca@protonmail.com>
2026-01-23 16:47:46 +0000

    Scala2 specs

--- modules/generic/src/test/scala/pureconfig/ProductHintSuite.scala
+++ modules/generic/src/test/scala/pureconfig/ProductHintSuite.scala
@@ -159,6 +159,27 @@ class ProductHintSuite extends BaseSuite {
     conf.getConfig("conf").to[Conf2] should failWith(UnknownKey("b"), "b", stringConfigOrigin(4))
   }
 
+  it should "disallow unknown keys even for types with no fields" in {
+    case class EmptyConf1()
+    case class EmptyConf2()
+    case object ObjConf1
+    case object ObjConf2
+
+    implicit val productHintEmptyConf = ProductHint[EmptyConf2](allowUnknownKeys = false)
+    implicit val productHintObjConf = ProductHint[ObjConf2.type](allowUnknownKeys = false)
+
+    val conf = ConfigFactory.parseString("""{
+      conf {
+        a = 1
+      }
+    }""")
+
+    conf.getConfig("conf").to[EmptyConf1] shouldBe Right(EmptyConf1())
+    conf.getConfig("conf").to[EmptyConf2] should failWith(UnknownKey("a"), "a", stringConfigOrigin(3))
+    conf.getConfig("conf").to[ObjConf1.type] shouldBe Right(ObjConf1)
+    conf.getConfig("conf").to[ObjConf2.type] should failWith(UnknownKey("a"), "a", stringConfigOrigin(3))
+  }
+
   it should "accumulate all failures if the product hint doesn't allow unknown keys" in {
     case class Conf(a: Int)
 
